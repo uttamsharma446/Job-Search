b998e9b436171248ab046a12e1c83d866f44c288
uttamsharma123 <suttam446@gmail.com>
2021-01-23 01:31:29 +0530

    finally done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function handleSearch() {
     var inputData = document.querySelector("#inputData").value.toUpperCase();
     var AllJob = document.getElementById("show-job")
     var jobs = AllJob.getElementsByClassName("job");
+    document.querySelector(".job-not-found").classList.add("display-none");
 
     for (var i = 0; i < jobs.length; i++) {
         var job = jobs[i].querySelector(".job-title").textContent.toUpperCase();
@@ -11,10 +12,10 @@ function handleSearch() {
             jobs[i].style.display="";
            
         }
-        else 
+        else  if(job.indexOf(inputData)===-1)
         {
+            
             jobs[i].style.display="none";
-           document.querySelector(".job-not-found").classList.remove("display-none");
         }
         
         
@@ -22,4 +23,24 @@ function handleSearch() {
 
 
 
+}
+function handleClick() {
+    var inputData = document.querySelector("#inputData").value.toUpperCase();
+    var AllJob = document.getElementById("show-job")
+    var jobs = AllJob.getElementsByClassName("job");
+    for (var i = 0; i < jobs.length; i++) {
+        var job = jobs[i].querySelector(".job-title").textContent.toUpperCase();
+      
+       console.log(job.indexOf(inputData));
+       if(job.indexOf(inputData)===-1)
+        {
+            
+          jobs[i].style.display="none";
+           document.querySelector(".job-not-found").classList.remove("display-none");
+  
+        }
+        
+        
+        
+    }
 }
