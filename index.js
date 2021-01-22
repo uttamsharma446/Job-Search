da09d4b66f0d22c49606f09ee732a7ab4ae59fb5
uttamsharma123 <suttam446@gmail.com>
2021-01-22 18:31:48 +0530

    start with navbar and search bar

---
+++ index.js
@@ -0,0 +1,12 @@
+const ele = document.createElement('div');
+ele.innerHTML = "<h1>Hello uttam </h1>";
+var arr=["uttam","hii","how ","sharma","where ","uttam"]
+var jobtitle=""
+for(var i=0;i<arr.length;i++)
+{
+    if(arr[i]==="uttam")
+    {
+        jobtitle+=arr[i]+"<br/>"
+    }
+    
+}
