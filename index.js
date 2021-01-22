4c17e5f32923a6cc772da3533930b1c2a7f5933e
uttamsharma123 <suttam446@gmail.com>
2021-01-23 00:30:08 +0530

    make job not found

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 function handleSearch() {
     var inputData = document.querySelector("#inputData").value.toUpperCase();
     var AllJob = document.getElementById("show-job")
-    var jobs = AllJob.getElementsByClassName("job")
+    var jobs = AllJob.getElementsByClassName("job");
+
     for (var i = 0; i < jobs.length; i++) {
         var job = jobs[i].querySelector(".job-title").textContent.toUpperCase();
+      
         if(job.indexOf(inputData)>-1)
         {
-            jobs[i].style.display=""
-        }
-        else if (job.indexOf(inputData)===-1)
-        {
-            jobs[i].style.display="none";
+            jobs[i].style.display="";
            
         }
+        else 
+        {
+            jobs[i].style.display="none";
+           document.querySelector(".job-not-found").classList.remove("display-none");
+        }
+        
         
     }
 
